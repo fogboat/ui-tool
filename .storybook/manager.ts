ebbecbf74b7db52d3f59a9aca8d9a90b804ff9c7
雾舟 <1345397147@qq.com>
2025-02-20 00:30:23 +0800

    配置修改

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -4,3 +4,31 @@ import theme from './theme';
 addons.setConfig({
   theme,
 });
+
+addons.register('TitleAddon', (api) => {
+  const PAGE_TITLE = 'UI-Tool'
+
+  const setTitle = () => {
+    let storyData
+    try {
+      storyData = api.getCurrentStoryData()      
+    } catch (e) {}
+    if (document?.title) {
+      document.title =
+      storyData !== null && storyData.title
+        ? `${storyData.title.replace(/\//g, ' / ')} - ${storyData.name} ⋅ ${PAGE_TITLE}`
+        : PAGE_TITLE
+    }
+    
+  }
+
+  return new MutationObserver(() => {
+    if (document.title.endsWith('Storybook')) {
+      setTitle();
+    }
+  }).observe(document.querySelector('title')!, {
+    childList: true,
+    subtree: true,
+    characterData: true,
+  });
+})
--- src/stories/useDialogs/useDialogs.stories.tsx
+++ src/stories/useDialogs/useDialogs.stories.tsx
@@ -93,27 +93,21 @@ const App = () => {
   return <DialogsProvider><Component /></DialogsProvider>
 
 }
-
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'useDialogs',
   component: App,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
 
 } satisfies Meta<typeof useDialogs>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
-
 export const Primary: Story = {
   args: {
-
   },
 
 
